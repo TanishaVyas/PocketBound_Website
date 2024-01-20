453a0e85706029ed670e0d3a24036574f12b1b46
TanishaVyas <122625790+TanishaVyas@users.noreply.github.com>
2024-01-20 12:30:13 +0530

    yayy

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,73 @@
-// MaterialUIComponent.js
-import React from "react";
-import { Grid, Paper, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Paper, Typography } from "@mui/material";
 import imageUrl from "..//Images//Temp.jpg";
 
-const MaterialUIComponent = () => {
+const About = () => {
+  const [screenSize, setScreenSize] = useState("3.5rem");
+
+  useEffect(() => {
+    const handleResize = () => {
+      const { innerWidth } = window;
+      let newSize = "";
+
+      if (innerWidth < 768) {
+        newSize = "2rem";
+      } else {
+        newSize = "3.5rem";
+      }
+
+      setScreenSize(newSize);
+    };
+
+    handleResize(); // Call once to set the initial screen size
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={6}>
-        <Paper style={{ backgroundColor: "#241b2d" }} elevation={0}>
+    <Paper style={{ backgroundColor: "#241b2d", padding: "16px" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: screenSize === "2rem" ? "column" : "row",
+        }}
+      >
+        <div
+          style={{
+            flex: "1",
+            marginRight: screenSize === "2rem" ? "0" : "16px",
+            flexDirection: "column",
+          }}
+        >
           <img src={imageUrl} style={{ maxWidth: "100%", height: "auto" }} />
-        </Paper>
-      </Grid>
-      <Grid item xs={6}>
-        <Paper style={{ backgroundColor: "#241b2d" }} elevation={0}>
-          <Typography variant="body1">
-            dfghjkl;ertyuiol;sdfvbnm,sdfghjktyuio
+        </div>
+        <div style={{ flex: "1", padding: "10px", flexDirection: "column" }}>
+          <Typography
+            variant="h5"
+            style={{
+              color: "#6492ff",
+              marginBottom: "16px",
+              fontSize: screenSize,
+            }}
+          >
+            About the Project
           </Typography>
-        </Paper>
-      </Grid>
-    </Grid>
+          <Typography variant="body1" style={{ color: "#fff" }}>
+            PocketBound transforms 3D games into mobile VR experiences, tackling
+            accessibility and affordability challenges. With a mobile-centric
+            approach, it utilizes existing phones, diverse microcontrollers, and
+            offers a DIY headset option. The project includes a game plugin for
+            easy one-click conversion of games to VR, aiming to democratize and
+            enhance the VR gaming scene through community-driven development in
+            the growing market.
+          </Typography>
+        </div>
+      </div>
+    </Paper>
   );
 };
 
-export default MaterialUIComponent;
+export default About;
