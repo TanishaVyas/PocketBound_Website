c799b6ce7cc2d5c6c5c1627a7a0b825e4b56da85
TanishaVyas <122625790+TanishaVyas@users.noreply.github.com>
2024-01-20 20:32:55 +0530

    ram

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,25 +9,39 @@ import {
   List,
   ListItem,
   ListItemText,
-  Hidden,
   IconButton,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import logo from "..//Images//logo.png";
-import { Link } from "react-scroll";
+import { Link as ScrollLink } from "react-scroll";
 
 function Navbar() {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const toggleDrawer = (open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-    setDrawerOpen(open);
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
   };
+
+  const handleLinkClick = (to) => () => {
+    const targetElement = document.getElementById(to);
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
+    }
+    setDrawerOpen(false);
+  };
+
+  const menuItems = [
+    { text: "Home", to: "home" },
+    { text: "About", to: "about" },
+    { text: "Features", to: "feature" },
+    { text: "Demo", to: "demo" },
+    { text: "Contact", to: "contact" },
+    // Add other menu items as needed
+  ];
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -37,18 +51,16 @@ function Navbar() {
         sx={{ backgroundColor: "#180F22" }}
       >
         <Toolbar>
-          <Hidden mdUp>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={toggleDrawer(true)}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Hidden>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
           <img
             src={logo}
             alt="Logo"
@@ -62,79 +74,42 @@ function Navbar() {
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            {/* Your existing desktop navigation buttons */}
-
-            <Button>
-              <Typography
-                variant="h6"
-                sx={{ fontSize: { xs: "16px", md: "20px" }, color: "white" }}
-              >
-                Home
-              </Typography>
-            </Button>
-            <Button href="#about">
-              <Typography
-                variant="h6"
-                sx={{ fontSize: { xs: "16px", md: "20px" }, color: "white" }}
-              >
-                About
-              </Typography>
-            </Button>
-            <Button href="#features">
-              <Typography
-                variant="h6"
-                sx={{ fontSize: { xs: "16px", md: "20px" }, color: "white" }}
-              >
-                Features
-              </Typography>
-            </Button>
-            <Button href="#demo">
-              <Typography
-                variant="h6"
-                sx={{ fontSize: { xs: "16px", md: "20px" }, color: "white" }}
-              >
-                demo
-              </Typography>
-            </Button>
-            <Button href="#contact">
-              <Typography
-                variant="h6"
-                sx={{ fontSize: { xs: "16px", md: "20px" }, color: "white" }}
-              >
-                contact
-              </Typography>
-            </Button>
-
-            {/* Add other desktop buttons as needed */}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {/* Responsive Drawer for smaller screens */}
-      <Hidden mdUp>
-        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
-          <List>
-            {[
-              { text: "Home", to: "home" },
-              { text: "About", to: "about" },
-              { text: "Features", to: "feature" },
-              { text: "Demo", to: "demo" },
-              { text: "Contact", to: "contact" },
-              // Add other menu items as needed
-            ].map((item, index) => (
-              <ListItem button key={index} onClick={toggleDrawer(false)}>
-                <Link
+            {menuItems.map((item, index) => (
+              <Button key={index} onClick={handleLinkClick(item.to)}>
+                <ScrollLink
                   to={item.to}
                   smooth={true}
                   duration={500}
                   offset={-70} // Adjust the offset as needed
                 >
-                  <ListItemText primary={item.text} />
-                </Link>
-              </ListItem>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      fontSize: { xs: "16px", md: "20px" },
+                      color: "white",
+                    }}
+                  >
+                    {item.text}
+                  </Typography>
+                </ScrollLink>
+              </Button>
             ))}
-          </List>
-        </Drawer>
-      </Hidden>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <List>
+          {menuItems.map((item, index) => (
+            <ListItem button key={index} onClick={handleLinkClick(item.to)}>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </Box>
   );
 }
